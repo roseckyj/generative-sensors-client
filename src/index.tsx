057ba115a8743a1baee2db58e068c5668f43d671
roseckyj <rosecky.jonas@centrum.cz>
2021-11-03 13:21:21 +0100

    Added https redirect and [OK]

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const location = window.location;
+
+if (location.protocol !== "https:" && !location.href.includes("localhost")) {
+    location.replace(
+        `https:${location.href.substring(location.protocol.length)}`
+    );
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
